ff0c6d2dcad050e9f0306d84263017db331787f6
Messier <810152974@qq.com>
2023-02-24 00:18:17 +0800

    新增第14个实例:阿里云盘的侧边导航栏

--- code/14/14.js
+++ code/14/14.js
@@ -1,8 +1,10 @@
+// 获取dom
 let items = document.querySelectorAll(".item");
 let current_tag = document.querySelector(".current-tag");
 let handler = document.querySelector(".handler");
 let left_box = document.querySelector(".left-box");
 
+// 先移除再添加active 改变current-tag的值
 function setActive() {
   items.forEach((item) => {
     item.classList.remove("active");
@@ -15,6 +17,7 @@ items.forEach((item) => {
   item.addEventListener("click", setActive);
 });
 
+// 点击handler 改变left-box的宽度
 handler.addEventListener("click", function () {
   if (!this.classList.contains("close")) {
     left_box.style.width = 0;
