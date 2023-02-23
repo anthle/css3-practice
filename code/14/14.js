a46ed78a3d216bfe5de8c2956a3555adb7035195
Messier <810152974@qq.com>
2023-02-23 23:59:11 +0800

    新增第14个实例:阿里云盘的侧边导航栏

---
+++ code/14/14.js
@@ -0,0 +1,26 @@
+let items = document.querySelectorAll(".item");
+let current_tag = document.querySelector(".current-tag");
+let handler = document.querySelector(".handler");
+let left_box = document.querySelector(".left-box");
+
+function setActive() {
+  items.forEach((item) => {
+    item.classList.remove("active");
+  });
+  this.classList.add("active");
+  current_tag.innerText = this.innerText;
+}
+
+items.forEach((item) => {
+  item.addEventListener("click", setActive);
+});
+
+handler.addEventListener("click", function () {
+  if (!this.classList.contains("close")) {
+    left_box.style.width = 0;
+    this.classList.add("close");
+  } else {
+    left_box.style.width = 250 + "px";
+    this.classList.remove("close");
+  }
+});
